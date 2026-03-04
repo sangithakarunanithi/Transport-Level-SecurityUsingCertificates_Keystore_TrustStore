c55b14a951d37f7b14c061f2a3ab056dae4379f9
Sangitha Karunanithi <sangiekaruna07@gmail.com>
2026-03-04 16:14:21 +0530

    Initial commit: Adding SSL and Keystore configuration

---
+++ src/main/java/com/example/trustStoreKeyStoreEg/TrustStoreKeyStoreEgApplication.java
@@ -0,0 +1,13 @@
+package com.example.trustStoreKeyStoreEg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustStoreKeyStoreEgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustStoreKeyStoreEgApplication.class, args);
+	}
+
+}
